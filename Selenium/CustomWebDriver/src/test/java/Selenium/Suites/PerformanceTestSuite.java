816fdc6b7b0ed54cbd84ef13aba38ff25aea6a3e
Paul Bruce <me@paulsbruce.io>
2017-11-05 11:14:31 -0500

    Added pre-test sanity check in test suite.

--- Selenium/CustomWebDriver/src/test/java/Selenium/Suites/PerformanceTestSuite.java
+++ Selenium/CustomWebDriver/src/test/java/Selenium/Suites/PerformanceTestSuite.java
@@ -2,12 +2,33 @@ package Selenium.Suites;
 
 import Selenium.Tests.*;
 import Selenium.utils.PerformanceTests;
+import com.google.common.io.CharStreams;
+import com.neotys.selenium.proxies.helpers.ModeHelper;
+import org.junit.BeforeClass;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import java.io.InputStreamReader;
+import java.net.URL;
+import static org.junit.Assert.assertTrue;
+
 
 @RunWith(Categories.class)
 @Categories.IncludeCategory(PerformanceTests.class)
 @Suite.SuiteClasses({DoPostV1_1a_fluent.class})
 public class PerformanceTestSuite {
+
+    @BeforeClass
+    public static void connectivityTest() throws Exception {
+
+        String baseUrl = ModeHelper.getSetting("baseUrl", "http://ushahidi");
+
+        URL url = new URL(baseUrl);
+        String body = CharStreams.toString(new InputStreamReader(url.openStream()));
+
+        assertTrue( // is our target app
+                String.format("The URL '%s' is unavailable or contains unexpected content.", baseUrl),
+                body.contains("ng-controller")
+        );
+    }
 }
